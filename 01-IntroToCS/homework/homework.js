2e7a74bed6d5e791b75ee8a9ca5c250309da6c7d
Joseph Godoy <josephgodoy.t@gmail.com>
2022-07-04 17:16:52 -0600

    second commit

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,21 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let suma = 0;
+  for (let i = 0; i < num.length ;i++){
+    suma = suma + num[i] * Math.pow(2, num.length-1 - i);
+  }
+  return suma;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let resto = '';
+  while (num !== 0) {
+    resto = num % 2 + resto;
+    num = Math.floor(num/2);
+  }
+  return resto;
 }
 
 
